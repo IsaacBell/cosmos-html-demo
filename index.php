e0eb2baeb83aaed5763f4ac17aebad9ce6ee50a4
Isaac Bell <isaacbell388@gmail.com>
2016-08-02 00:09:28 -0500

    Cleanup

--- index.php
+++ index.php
@@ -10,18 +10,9 @@
     <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
-    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="font-awesome-4.2.0/css/font-awesome.css"> -->
     <link rel="stylesheet" type="text/css" href="css/normalize.css">
     <link rel="stylesheet" type="text/css" href="css/button.css">
-    <!-- <link rel="stylesheet" type="text/css" href="css/jasny-bootstrap.min.css"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="css/arrow.css"> -->
-    <!-- <link href="css/owl.carousel.css" rel="stylesheet" media="screen"> -->
-    <!-- <link href="css/owl.theme.css" rel="stylesheet" media="screen"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="css/responsive.css"> -->
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.0/animate.min.css">
-    <!-- <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> -->
     <script type="text/javascript" src="js/modernizr.custom.js"></script>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -29,14 +20,6 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-XXXXXXXX-X', 'auto');
-    ga('send', 'pageview');
-    </script>
   </head>
   <body>
     <a href="http://outerhavenrecords.com" target="_blank" class="signature">Back to Outer Haven</a>
@@ -44,17 +27,6 @@
     <button class="explore">Return<span class="icon-right"></span><span class="icon-right after"></span></button>
   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- <script type="text/javascript" src="js/jquery.1.11.1.js"></script> -->
-    <!-- <script type="text/javascript" src="js/bootstrap.js"></script> -->
-    <!-- <script type="text/javascript" src="js/SmoothScroll.js"></script> -->
-    <!-- <script type="text/javascript" src="js/jasny-bootstrap.min.js"></script> -->
-    <!-- <script src="js/owl.carousel.js"></script> -->
-    <!-- <script src="js/scripts.js"></script> -->
-    <!-- <script src="js/animatedModal.min.js"></script> -->
-    <!-- <script type="text/javascript" src="http://andywer.github.io/jquery-dim-background/jquery.dim-background.min.js"></script> -->
-    
-    
-    <!-- <script type="text/javascript" src="js/main.js"></script> -->
     <script type="text/javascript" src="js/canvas.js"></script>
   </body>
 </html>
